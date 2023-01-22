89f9144bcdc53e8f0b421fbc622d881e7245be28
SirFlaps <73776518+SirFlaps@users.noreply.github.com>
2023-01-22 20:24:53 +0100

    Add files via upload

---
+++ Sim pedals.ino
@@ -0,0 +1,30 @@
+ #include "Joystick.h"
+#include <Q2HX711.h>
+Q2HX711 hx711(A5, A4);
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
+  JOYSTICK_TYPE_JOYSTICK, 0, 0, false, false, true, false, false, false, false, true, false, true, false);
+
+
+  int RzAxis;
+  int brake;
+  int throttle;
+
+  
+  void setup()
+{
+  Joystick.begin();
+
+  Serial.begin(9600);
+}
+  void loop()
+  {     
+        Joystick.setZAxis(analogRead(A2));
+        Joystick.setThrottle(analogRead(A0));
+        Joystick.setBrake(hx711.read()/30000);
+        Serial.println(hx711.read()/30000);
+
+       delay(0.1);
+  }     
+
+  
